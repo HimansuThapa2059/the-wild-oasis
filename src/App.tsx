a92a1147225803120c29c47d63be4dad5ebc2395
Himansu <himansuthapa2059@gmail.com>
2024-05-18 15:54:17 +0545

    added app layout

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Login from "@/pages/Login";
 import PageNotFound from "@/pages/PageNotFound";
 import { FC } from "react";
 import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
 
 const App: FC = () => {
   return (
@@ -16,13 +17,15 @@ const App: FC = () => {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="booking" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
-          <Route path="users" element={<Users />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="account" element={<Account />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="booking" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
