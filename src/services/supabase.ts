99596efd3b5cbe6093dfa27bfef0bd1e344a253e
Himansu <himansuthapa2059@gmail.com>
2024-05-19 22:30:57 +0545

    connected supabase with react app

---
+++ src/services/supabase.ts
@@ -0,0 +1,9 @@
+import { Database } from "@/data/types/database.types";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://zlygckncitbvysugssvx.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpseWdja25jaXRidnlzdWdzc3Z4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYwNDE5ODcsImV4cCI6MjAzMTYxNzk4N30.dANBd2c1-8GB90O_GaKRoW_RGEasYjZ54OtoOpTIdLQ";
+const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+
+export default supabase;
